58fe88957462b89f4256600323be0deed0bee27d
perkarisandeep <sperkari@lear.com>
2019-04-26 15:09:20 -0400

    GraphQL learnings

---
+++ server/Readme.md
@@ -0,0 +1,4 @@
+1. First create the schemas
+    a. Create the Types of the Query
+    b. Create the Query for the main object in the response.
+    
---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql');
+
+
+const app = express();
+
+
+app.use('/graphql', graphqlHTTP({
+    
+}))
+
+app.listen(4000, () =>{
+    console.log(`now listen for requests on port 4000`)
+})
